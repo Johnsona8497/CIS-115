40a0586bdb8264e861535e2fd6a470a62e9a7b48
Johnsona8497 <55244455+Johnsona8497@users.noreply.github.com>
2019-09-12 10:44:04 -0400

    Add files via upload

--- m2lab_Johnson.py
+++ m2lab_Johnson.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+M2LAB
+Johnson
+9/12/19
+"""
+
 ##// M2LAB
-##// Johnson
+##// Anthony Johnson
 ##// 9/12/19
 ##
 ##// Greet the user by name,
@@ -11,14 +18,21 @@
 ##Declare Interger age
 ##
 ##//Ask for their name
-##Display "What is your name?"
+print("What is your name?")
+
 ##Input Name
-##
+name=input()
+
 ##//Ask for their age
-print("What is your age?"
+print("What is your age?")
+
 ##Input Age
-##
+age=int(input())
+
 ##//tell them (by name) how old they will be
 ##//on their next birthday
 ##Set age=age+1
+age=age+1
 ##Display "Well, ", name, "on your next birthday you will be ", age, "!"
+print("Your name is", name)
+print("You will be",age,"on your next birthday")
